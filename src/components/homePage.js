8efc3c313cbc4e41144221cb094dc3ac7c851b4f
Shashwat Singh Ranka <shashwatsinghranka@Shashwats-MacBook-Pro.local>
2024-03-16 01:26:33 +0530

    infinite-scroll

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "../App.css";
-import BackgroundImage from "../images/bg.png";
+// import BackgroundImage from "../images/bg.png";
+// import CardComponent from "./cardComponent";
+import InfiniteListComponent from "./infiniteList";
+import { v4 as uuidv4 } from "uuid";
 
 const HomePage = () => {
+  const createItems = (length = 100) =>
+    Array.from({ length }).map(() => uuidv4());
+
+  const [data, setData] = useState(createItems());
+  const [isLoading, setIsLoading] = useState(false);
+
+  const loadMore = async (length = 100) =>
+    new Promise((res) => setTimeout(() => res(createItems(length)), 500));
+
+  const next = async (direction) => {
+    try {
+      setIsLoading(true);
+      const newData = await loadMore();
+
+      setData((prev) =>
+        direction === "right" ? [...newData, ...prev] : [...prev, ...newData],
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <header style={HeaderStyle}>
+    <div style={HeaderStyle}>
       <h1 className="main-title text-center">login / register page</h1>
       <p className="main-para text-center">join us now and don't waste time</p>
       <div className="buttons text-center">
@@ -19,7 +44,18 @@ const HomePage = () => {
           </button>
         </Link>
       </div>
-    </header>
+
+      <div className="m-5-auto text-center">
+        <h2>Welcome to our website</h2>
+        <h5>Explore our services</h5>
+      </div>
+      <InfiniteListComponent
+        isLoading={isLoading}
+        items={data}
+        canLoadMore={true}
+        next={next}
+      />
+    </div>
   );
 };
 
@@ -28,7 +64,6 @@ export default HomePage;
 const HeaderStyle = {
   width: "100%",
   height: "100vh",
-  background: `url(${BackgroundImage})`,
   backgroundPosition: "center",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
