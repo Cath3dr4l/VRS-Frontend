c4ff4abe405402c05f4c8c3465f0f661e362f884
Shashwat Singh Ranka <shashwatsinghranka@Shashwats-MacBook-Pro.local>
2024-03-20 18:49:03 +0530

    searchbar

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,20 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import InfiniteListComponent from "./infiniteList";
+import CardComponent from "./cardComponent";
 
-const SearchBar = ({
-  isLoading,
-  items,
-  canLoadMore,
-  next,
-  renderComponent,
-}) => {
-  const [query, setQuery] = useState("");
+const SearchBar = ({ videosPath }) => {
+  const [videos, setVideos] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [canLoadMore, setCanLoadMore] = useState({ right: true });
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const response = await fetch(videosPath, { method: "GET" });
+      const json = await response.json();
+      setVideos(json);
+      setData(json.slice(0, 25)); // Store the first 25 videos in data
+    };
+    fetchVideos();
+  }, []);
+
   const keys = ["name"];
 
-  const search = (data) => {
+  const search = (data, query) => {
     return data.filter((row) =>
       keys.some((key) => row[key].toLowerCase().includes(query.toLowerCase())),
     );
+  };
+
+  const fetchMoreVideos = async (last, length = 25) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const newData = videos.slice(last, last + length);
+        resolve(newData);
+      }, 1000);
+    });
+  };
+
+  const next = async () => {
+    const last = data.length;
+    console.log(last);
+    setIsLoading(true);
+    const newData = await fetchMoreVideos(last);
+    setData((prev) => [...prev, ...newData]);
+    setIsLoading(false);
+    if (data.length === videos.length) {
+      setCanLoadMore({ right: false });
+    }
   };
 
   return (
@@ -22,15 +53,29 @@ const SearchBar = ({
       <input
         type="text"
         placeholder="Search..."
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={(e) => {
+          if (e.target.value.length !== 0) {
+            setIsSearching(true);
+          }
+          if (e.target.value.length === 0) {
+            setIsSearching(false);
+          }
+          setData(search(videos, e.target.value));
+        }}
       />
-      <InfiniteListComponent
-        items={search(items)}
-        isLoading={isLoading}
-        canLoadMore={canLoadMore}
-        next={next}
-        renderComponent={renderComponent}
-      />
+
+      {isSearching && (
+        <InfiniteListComponent
+          style={{ scrollbars: "false" }}
+          isLoading={isLoading}
+          items={data}
+          canLoadMore={canLoadMore}
+          next={next}
+          renderComponent={(item) => (
+            <CardComponent item={item} key={item.id} />
+          )}
+        />
+      )}
     </div>
   );
 };
