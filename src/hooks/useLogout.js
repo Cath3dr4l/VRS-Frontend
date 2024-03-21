abe649405d49f141c865a6082d934c7b7ecdc882
harshit-jain52 <harshitjain0502@gmail.com>
2024-03-21 09:46:52 +0530

    navigate on logout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,14 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
+  const navigate = useNavigate();
   const logout = () => {
     // remove user from storage
     localStorage.removeItem("customer");
     localStorage.removeItem("videodog-cart");
+    navigate("/login");
 
     // update the auth context
     dispatch({ type: "LOGOUT" });
