9b48f390784425598888fede4f003020005e3dca
harshit-jain52 <harshitjain0502@gmail.com>
2024-03-28 14:50:36 +0530

    Styles on Profile page, can edit name & address

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { formatDistanceToNow, format, add } from "date-fns";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faSave } from "@fortawesome/free-solid-svg-icons";
 
 const Profile = () => {
   const { customer } = useAuthContext();
   const [profile, setProfile] = useState(null);
   const [orders, setOrders] = useState([]);
   const [error, setError] = useState(null);
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [isEditingAddress, setIsEditingAddress] = useState(false);
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -22,6 +28,8 @@ const Profile = () => {
       }
       if (response.ok) {
         setProfile(data);
+        setName(data.name);
+        setAddress(data.address);
         setError(null);
       }
     };
@@ -49,50 +57,148 @@ const Profile = () => {
     }
   }, [customer]);
 
+  const updateProfile = async () => {
+    const response = await fetch("/api/customers/profile", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${customer.token}`,
+      },
+      body: JSON.stringify({ name }),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      setProfile({ ...profile, name, address });
+      setTimeout(() => alert("Profile updated successfully"), 500);
+    } else {
+      alert(`An error occured: ${data.error}`);
+    }
+  };
+
   return (
-    <div style={{ color: "white " }}>
-      <h2>Profile</h2>
-      <div className="customer-details">
-        {profile && (
-          <div>
-            <h3>{profile.name}</h3>
-            <p>{profile.email}</p>
-            <p>{profile.phone}</p>
-          </div>
-        )}
-      </div>
-      <h2>Orders</h2>
-      <div className="orders">
-        {orders &&
-          orders.map((order) => (
-            <div key={order._id}>
-              <p>{order.videoID.name}</p>
-              <p>Qty: {order.quantity}</p>
-              <p>Status: {order.status}</p>
-              <p>Price: {order.price}</p>
-              {order.status !== "bought" && (
+    <>
+      <div className="flex flex-col h-screen">
+        <h1 className="text-4xl font-bold text-text text-center py-8">
+          Profile
+        </h1>
+        <div className="text-white flex justify-between flex-grow">
+          <div className="w-1/4 bg-gray-900 text-white p-6 shadow-lg">
+            <h2 className="text-2xl font-bold mb-4">Profile Details</h2>
+            {profile && (
+              <dl className="space-y-2 text-lg">
                 <div>
-                  <p>Rented for: {order.duration} weeks</p>
-                  <p>
-                    Due Date:
-                    {format(
-                      add(new Date(order.createdAt), { weeks: order.duration }),
-                      "dd/MM/yyyy"
+                  <dt className="font-semibold">Username:</dt>
+                  <dd className="font-normal">{profile.username}</dd>
+                </div>
+                <div>
+                  <dt className="font-semibold">Name:</dt>
+                  {isEditingName ? (
+                    <input
+                      className="text-gray-900 w-1/2"
+                      type="text"
+                      defaultValue={profile.name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  ) : (
+                    <span className="font-normal">{profile.name}</span>
+                  )}
+                  <button
+                    className="text-white px-2 py-1 ml-2"
+                    onClick={() => {
+                      if (isEditingName) {
+                        setIsEditingName(false);
+                        updateProfile();
+                      } else {
+                        setIsEditingName(true);
+                      }
+                    }}
+                  >
+                    {isEditingName ? (
+                      <FontAwesomeIcon icon={faSave} />
+                    ) : (
+                      <FontAwesomeIcon icon={faEdit} />
                     )}
+                  </button>
+                </div>
+                <div>
+                  <dt className="font-semibold">Email:</dt>
+                  <dd className="font-normal">{profile.email}</dd>
+                </div>
+                <div>
+                  <dt className="font-semibold">Phone:</dt>
+                  <dd className="font-normal">{profile.phone}</dd>
+                </div>
+                <div>
+                  <dt className="font-semibold">Name:</dt>
+                  {isEditingAddress ? (
+                    <input
+                      className="text-gray-900 w-1/2"
+                      type="text"
+                      defaultValue={profile.address}
+                      onChange={(e) => setAddress(e.target.value)}
+                    />
+                  ) : (
+                    <span className="font-normal">
+                      {profile.address ? profile.address : "Not Provided"}
+                    </span>
+                  )}
+                  <button
+                    className="text-white px-2 py-1 ml-2"
+                    onClick={() => {
+                      if (isEditingAddress) {
+                        setIsEditingAddress(false);
+                        updateProfile();
+                      } else {
+                        setIsEditingAddress(true);
+                      }
+                    }}
+                  >
+                    {isEditingAddress ? (
+                      <FontAwesomeIcon icon={faSave} />
+                    ) : (
+                      <FontAwesomeIcon icon={faEdit} />
+                    )}
+                  </button>
+                </div>
+              </dl>
+            )}
+          </div>
+          <div>
+            <h2 className="text-2xl font-bold mr-2">Orders</h2>
+            {orders &&
+              orders.map((order) => (
+                <div key={order._id}>
+                  <p>{order.videoID.name}</p>
+                  <p>Qty: {order.quantity}</p>
+                  <p>Status: {order.status}</p>
+                  <p>Price: {order.price}</p>
+                  {order.status !== "bought" && (
+                    <div>
+                      <p>Rented for: {order.duration} weeks</p>
+                      <p>
+                        Due Date:
+                        {format(
+                          add(new Date(order.createdAt), {
+                            weeks: order.duration,
+                          }),
+                          "dd/MM/yyyy"
+                        )}
+                      </p>
+                    </div>
+                  )}
+                  <p>
+                    Ordered:
+                    {formatDistanceToNow(new Date(order.createdAt), {
+                      addSuffix: true,
+                    })}
                   </p>
                 </div>
-              )}
-              <p>
-                Ordered:
-                {formatDistanceToNow(new Date(order.createdAt), {
-                  addSuffix: true,
-                })}
-              </p>
-            </div>
-          ))}
+              ))}
+          </div>
+        </div>
       </div>
       {error && <div className="error">{error}</div>}
-    </div>
+    </>
   );
 };
 
