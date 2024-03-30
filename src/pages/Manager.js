0ce4193c6a0b5e32321a92b153c75c969828f4d0
harshit-jain52 <harshitjain0502@gmail.com>
2024-03-30 10:59:45 +0530

    Component for repetitve code

--- src/pages/Manager.js
+++ src/pages/Manager.js
@@ -1,30 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import StaffSearchList from "../components/staffSearchList";
-import { FaAngleDown } from "react-icons/fa";
+import NavButton from "../components/NavButton";
 
 const Manager = () => {
   return (
     <div>
       <div className="z-[100] fixed ml-[200px] m-5 flex gap-4">
-        <Link to="/management/manageorders">
-          <button className="group relative overflow-hidden font-lg text-xl font-semibold text-white cursor-pointer flex items-center">
-            All Orders
-            <span className="absolute h-0.5 bg-white bottom-0 left-0 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-400 ease-out"></span>
-          </button>
-        </Link>
-        <Link to="/management/recruit">
-          <button className="group relative overflow-hidden font-lg text-xl font-semibold text-white cursor-pointer flex items-center">
-            Recruit Staff
-            <span className="absolute h-0.5 bg-white bottom-0 left-0 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-400 ease-out"></span>
-          </button>
-        </Link>
-        <Link to="/management/addmovie">
-          <button className="group relative overflow-hidden font-lg text-xl font-semibold text-white cursor-pointer flex items-center">
-            Add Movie
-            <span className="absolute h-0.5 bg-white bottom-0 left-0 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-400 ease-out"></span>
-          </button>
-        </Link>
+        <NavButton label="All Orders" path="/management/manageorders" />
+        <NavButton label="Recruit Staff" path="/management/recruit" />
+        <NavButton label="Add Movie" path="/management/addmovie" />
       </div>
       <div className="py-20">
         <StaffSearchList videosPath="/api/movies" />
--- src/pages/Staff.js
+++ src/pages/Staff.js
@@ -1,17 +1,11 @@
-import { Link } from "react-router-dom";
 import StaffSearchList from "../components/staffSearchList";
-import { FaAngleDown } from "react-icons/fa";
+import NavButton from "../components/NavButton";
 
 const Staff = () => {
   return (
     <div>
       <div className="z-[100] fixed ml-[200px] m-5 flex gap-4">
-        <Link to="/management/allorders">
-          <button className="group relative overflow-hidden font-lg text-xl font-semibold text-white cursor-pointer flex items-center">
-            All Orders
-            <span className="absolute h-0.5 bg-white bottom-0 left-0 w-full transform scale-x-0 group-hover:scale-x-100 transition-transform duration-400 ease-out"></span>
-          </button>
-        </Link>
+        <NavButton label="All Orders" path="/management/allorders" />
       </div>
       <div className="py-20">
         <StaffSearchList videosPath="/api/movies" />
