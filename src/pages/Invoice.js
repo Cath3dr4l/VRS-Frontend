27471a6a93d28434128b1308c88ebaef4643aa79
Pranaya <120286180+pransverse@users.noreply.github.com>
2024-04-15 16:31:14 +0530

    invoice done

--- src/pages/Invoice.js
+++ src/pages/Invoice.js
@@ -14,6 +14,50 @@ import { format } from "date-fns";
 import { useEffect, useState } from "react";
 import font from "../fonts/brandon-grotesque-black-58a8a3e824392.otf";
 
+import React from "react";
+import { Svg, Path } from "@react-pdf/renderer";
+
+function IconMailFill(props) {
+  return (
+    <Svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ maxWidth: "14px" }}
+      {...props}
+    >
+      <Path fill="none" d="M0 0h24v24H0z" />
+      <Path d="M3 3h18a1 1 0 011 1v16a1 1 0 01-1 1H3a1 1 0 01-1-1V4a1 1 0 011-1zm9.06 8.683L5.648 6.238 4.353 7.762l7.72 6.555 7.581-6.56-1.308-1.513-6.285 5.439z" />
+    </Svg>
+  );
+}
+
+function IconPhone(props) {
+  return (
+    <Svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      style={{ maxWidth: "13px" }}
+      {...props}
+    >
+      <Path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64c0 247.4 200.6 448 448 448 18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368c-70.4-33.3-127.4-90.3-160.7-160.7l49.3-40.3c13.7-11.2 18.4-30 11.6-46.3l-40-96z" />
+    </Svg>
+  );
+}
+
+function IconMailSendFill(props) {
+  return (
+    <Svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ maxWidth: "14px" }}
+      {...props}
+    >
+      <Path fill="none" d="M0 0h24v24H0z" />
+      <Path d="M2 5.5V3.993A1 1 0 012.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 01-.992.993H2.992A.993.993 0 012 20.007V19h18V7.3l-8 7.2-10-9zM0 10h5v2H0v-2zm0 5h8v2H0v-2z" />
+    </Svg>
+  );
+}
+
 Font.register({
   family: "Brandon-Grotesque-Black",
   src: font,
@@ -119,8 +163,28 @@ const Address = () => (
   <View style={styles.titleContainer}>
     <View style={styles.spaceBetween}>
       <View>
-        <Text style={styles.addressTitle}>DataDog HQ, </Text>
-        <Text style={styles.addressTitle}>IIT Kharagpur</Text>
+        <Text style={styles.addressTitle}>CONTACT US AT: </Text>
+        <View
+          style={{ flexDirection: "row", alignItems: "center", gap: "5px" }}
+        >
+          <IconPhone /> <Text style={styles.address}> 9606990317</Text>
+        </View>
+        <View
+          style={{ flexDirection: "row", alignItems: "center", gap: "5px" }}
+        >
+          <IconMailFill />{" "}
+          <Text style={styles.address}>datadoghps@gmail.com </Text>
+        </View>
+        <View
+          style={{ flexDirection: "row", alignItems: "flex-start", gap: "5px" }}
+        >
+          <IconMailSendFill />
+          <View>
+            <Text style={styles.address}>DataDog HQ, </Text>
+            <Text style={styles.address}>{"\t"}IIT Kharagpur,</Text>
+            <Text style={styles.address}>West Bengal, India</Text>
+          </View>
+        </View>
       </View>
     </View>
   </View>
